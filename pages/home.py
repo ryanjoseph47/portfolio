4025283c3637becf44e8795cd65ba0ca2dd0623c
Ryan J <ryan.joseph@student.mec.cuny.edu>
2025-11-16 06:05:19 -0500

    Create home page

---
+++ pages/home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+st.markdown("""
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.8/js/bootstrap.min.js" integrity="sha512-nKXmKvJyiGQy343jatQlzDprflyB5c+tKCzGP3Uq67v+lmzfnZUi/ZT+fc6ITZfSC5HhaBKUIvr/nTLCV+7F+Q==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+""", unsafe_allow_html=True)
+
+with open('static/main.css') as f:
+  css = f.read()
+
+st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
+st.html("assets/files/home.html")
